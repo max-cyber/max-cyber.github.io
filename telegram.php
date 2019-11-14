c28a401a728b71a1ea34e76051f8358fec14fd85
max-cyber <56264173+max-cyber@users.noreply.github.com>
2019-11-14 20:08:17 +0200

    Add files via upload

---
+++ telegram.php
@@ -0,0 +1,22 @@
+<?php
+$name = $_POST['user_name'];
+$name = $_POST['user_phone'];
+$token = "1008873416:AAF85LR-umJSMFRtxY5sebc7kLR8qT0G40U";
+$chat_id = "-325290915";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Заявка на консультацию.'
+);
+foreach ($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$text}","r");
+
+if ($sendToTelegram) {
+  header('Location: index.html');
+} else {
+  echo '<h1 class="danger">Ошибка</h1>';
+}
+}
+?>
