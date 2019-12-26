051fc9fc74f9acf51a3cb24e19dcd6a61875c0b9
max-cyber <56264173+max-cyber@users.noreply.github.com>
2019-12-26 12:53:00 +0200

    Add files via upload

---
+++ js/xmas.js
@@ -0,0 +1,1 @@
+//$(function(){$(".header-img").attr("src","img/logo-xmas.svg");});$(function(){$(".xmas").attr("src","../img/logo-xmas.svg");});$(function(){$(".xmas-2").attr("src","../../img/logo-xmas.svg");});
